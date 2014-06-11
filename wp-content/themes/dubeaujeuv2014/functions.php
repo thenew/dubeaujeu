c93e490ca6b228f05fb487a0a7b19ef0d5ac3460
Rémy Barthez <remy@thenew.fr>
2014-06-11 14:32:01 +0200

    wip redirect

--- wp-content/themes/dubeaujeuv2014/functions.php
+++ wp-content/themes/dubeaujeuv2014/functions.php
@@ -1,9 +1,9 @@
 <?php
 require_once(get_template_directory().'/fon/core/bootstrap.php');
 
-// add_action('init', 'wip_redirect');
+add_action('template_redirect', 'wip_redirect');
 function wip_redirect() {
-    if( ! is_admin() && strstr($_SERVER['REQUEST_URI'] , 'wp-admin') === FALSE && strstr($_SERVER['REQUEST_URI'] , 'wp-login') === FALSE && strstr($_SERVER['REQUEST_URI'] , 'art/illustrations') === FALSE) {
+    if( ! is_admin() && ! is_attachment() && strstr($_SERVER['REQUEST_URI'] , 'wp-admin') === FALSE && strstr($_SERVER['REQUEST_URI'] , 'wp-login') === FALSE && strstr($_SERVER['REQUEST_URI'] , 'art/illustrations') === FALSE) {
         wp_redirect( '/art/illustrations/' );
         die;
     }

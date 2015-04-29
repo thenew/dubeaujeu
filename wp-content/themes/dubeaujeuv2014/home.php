bad52a46309d65e59bece824e26b4fb38ce9bdf4
Rémy <remy@thenew.fr>
2015-04-29 19:34:15 +0200

    home landing

---
+++ wp-content/themes/dubeaujeuv2014/home.php
@@ -0,0 +1,20 @@
+<?php
+get_header();
+?>
+<div class="landing">
+    <div class="logo">
+        <div class="icon"></div>
+        <div class="title">
+            <div>du</div>
+            <div>bEau</div>
+            <div>jeu</div>
+        </div>
+    </div>
+    <div class="intro">
+        <em>Du Beau Jeu</em> a pour but de partager le plus beau du monde du jeu vidéo et promouvoir ses artistes.<br>
+        En attandant, suivez-nous sur <a href="http://twitter.com/dubeaujeu">twitter</a>.<br>
+        シ
+    </div>
+</div>
+<?php
+get_footer();

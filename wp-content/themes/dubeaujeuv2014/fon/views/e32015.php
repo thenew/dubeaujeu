bdb3715ac68b6ce7e8e53eb2d6d8042b236ac6fb
Rémy <remy@thenew.fr>
2015-04-29 18:37:07 +0200

    #e3 fix logo

--- wp-content/themes/dubeaujeuv2014/fon/views/e32015.php
+++ wp-content/themes/dubeaujeuv2014/fon/views/e32015.php
@@ -1,12 +1,22 @@
 <?php
+
+// CSS
+
 function fon_styles_e32015() {
     wp_enqueue_style( 'e32015', ASSETS_URL.'/css/pages/e32015.css' );
 }
 add_action( 'wp_enqueue_scripts', 'fon_styles_e32015' );
 
+// VIEW
+
 get_header();
-require_once TEMPLATEPATH.'/assets/img/e32015/logo.svg';
 ?>
+<div class="e32015-page">
+    <div class="logo-box">
+        <?php require_once TEMPLATEPATH.'/assets/img/e32015/logo.svg'; ?>
+    </div>
+</div>
+
 <script>
 
 jQuery(window).load(function(e){
@@ -30,13 +40,13 @@ jQuery(window).load(function(e){
 
               // .to(paths.eq(1), 0.15, {strokeDashoffset: 0}, "+=.15")
             tl = new TimelineMax(),
-            tl.to(paths.eq(0), 0.2, {strokeDashoffset: 0})
-              .to(paths.eq(1), 0.2, {strokeDashoffset: 0}, "+=.1")
-              .to(paths.eq(2), 0.2, {strokeDashoffset: 0}, "+=.1")
-              .to(paths.eq(3), 0.2, {strokeDashoffset: 0}, "+=.1")
-              .to(paths.eq(4), 0.25, {strokeDashoffset: 0}, "+=.2")
-              .to(paths.eq(5), 0.4, {strokeDashoffset: 0})
-              .to(paths.eq(6), 0.2, {strokeDashoffset: 0}, "+=.2")
+            tl.to(paths.eq(0), 0.1, {strokeDashoffset: 0})
+              .to(paths.eq(1), 0.1, {strokeDashoffset: 0}, "+=.05")
+              .to(paths.eq(2), 0.1, {strokeDashoffset: 0}, "+=.05")
+              .to(paths.eq(3), 0.1, {strokeDashoffset: 0}, "+=.05")
+              .to(paths.eq(4), 0.2, {strokeDashoffset: 0}, "+=.25")
+              .to(paths.eq(5), 0.3, {strokeDashoffset: 0})
+              .to(paths.eq(6), 0.2, {strokeDashoffset: 0}, "+=.25")
               .to(paths.eq(7), 0.15, {strokeDashoffset: 0})
               .to(paths.eq(8), 0.15, {strokeDashoffset: 0})
               .to(paths.eq(9), 0.15, {strokeDashoffset: 0})

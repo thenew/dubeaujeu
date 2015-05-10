8a31ee1633f26682d00c0833a4089784adaf11de
Rémy Barthez <remy@thenew.fr>
2015-05-11 00:10:36 +0200

    e32015 planning

--- wp-content/themes/dubeaujeuv2014/pages/e32015.php
+++ wp-content/themes/dubeaujeuv2014/pages/e32015.php
@@ -16,8 +16,81 @@ get_header();
     <div class="logo-box">
         <?php require_once TEMPLATEPATH.'/assets/img/e32015/logo.svg'; ?>
     </div>
+
+    <div class="e32015-planning">
+        <div class="day">
+            <div class="date">
+                <div class="day">15</div>
+                <div class="month">juin</div>
+            </div>
+            <div class="items">
+                <div class="item">
+                    <div class="title">Bethesda</div>
+                    <div class="hours">4 h (France - UTC+1)</div>
+                    <div class="hours">4 h (Los Angeles - UTC-8)</div>
+                    <a class="link" href="http://www.twitch.tv/bethesda" target="_blank">twitch.tv/bethesda</a>
+                </div>
+                <div class="item">
+                    <div class="title">Electronic Arts</div>
+                    <div class="hours">de 22 h à 23 h (France - UTC+1)</div>
+                </div>
+            </div>
+        </div>
+        <div class="day">
+            <div class="date">
+                <div class="day">16</div>
+                <div class="month">juin</div>
+            </div>
+            <div class="items">
+                <div class="item">
+                    <div class="title">Square Enix</div>
+                    <div class="hours">de 18 h à 19 h (France - UTC+1)</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="nc">
+            <div class="items">
+                <div class="item">Sony : inconnu</div>
+                <div class="item">Nintendo : inconnu</div>
+                <div class="item">Microsoft : inconnu</div>
+                <div class="item">Ubisoft : inconnu</div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
+/*
+Pour l’instant, les horaires (fr) des confs E3 confirmées :
 
+Sony : inconnu
+Nintendo : inconnu
+Microsoft : inconnu
+Ubisoft : inconnu
+
+Bethesda : 15 juin à 4h
+http://www.twitch.tv/bethesda
+
+Electronic Arts : 15 juin de 22h à 23h
+Square Enix : 16 Juin de 18h à 19h
+AMD PC Gaming Show :  17 juin de 2h à 5h
+http://www.pcgamingshow.com/
+
+Konami ?
+
+15 juin
+Bethesda : à 4h
+EA : de 22h à 23h
+
+16 juin
+Square Enix : de 18h à 19h
+
+17 juin
+AMD PC Gaming Show :  de 2h à 5h
+
+
+Expo : 16 - 18 juin
+
+*/
 get_footer();

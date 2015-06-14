22e522979f34b6118dca9f853bcb6e6f9d1c2653
thenew <remy@thenew.fr>
2015-06-14 20:04:35 +0200

    e3mosaic

---
+++ wp-content/themes/dubeaujeuv2014/assets/js/pages/e32015mosaic.js
@@ -0,0 +1,375 @@
+jQuery(window).ready(function(e){
+
+    // svg logo
+    var svg = jQuery('svg');
+    svg.each(function(i, el) {
+        var paths = jQuery(el).find('path:not(defs path)');
+
+        paths.each(function(i, e) {
+            e.style.strokeDasharray = e.style.strokeDashoffset = e.getTotalLength();
+        });
+    });
+
+    // packery
+    var $container = jQuery('.bricks');
+    // init
+    $container.packery({
+      itemSelector: '.brick-item',
+      percentPosition: true,
+      transitionDuration: '0s',
+      stamp: '.stamp',
+      gutter: 0
+    });
+
+});
+
+jQuery(window).load(function(e){
+    jQuery('.js-hidden').removeClass('js-hidden');
+
+    var svg = jQuery('.handfont');
+
+    svg.each(function(i, el) {
+
+        var paths = jQuery(el).find('path:not(defs path)');
+
+        // For each path, set the stroke-dasharray and stroke-dashoffset
+        // equal to the path's total length, hence rendering it invisible
+        // paths.each(function(i, e) {
+        //     e.style.strokeDasharray = e.style.strokeDashoffset = e.getTotalLength();
+        // });
+
+        // Add each separate line animation to the timeline, animating the
+        // stroke-dashoffset to 0. Use the duration, delay and easing to
+        // achieve the perfect animation.
+        setTimeout(function() {
+
+              // .to(paths.eq(1), 0.15, {strokeDashoffset: 0}, "+=.15")
+            tl = new TimelineMax(),
+            tl.to(paths.eq(0), 0.1, {strokeDashoffset: 0})
+              .to(paths.eq(1), 0.1, {strokeDashoffset: 0}, "+=.05")
+              .to(paths.eq(2), 0.1, {strokeDashoffset: 0}, "+=.05")
+              .to(paths.eq(3), 0.1, {strokeDashoffset: 0}, "+=.05")
+              .to(paths.eq(4), 0.2, {strokeDashoffset: 0}, "+=.25")
+              .to(paths.eq(5), 0.3, {strokeDashoffset: 0})
+              .to(paths.eq(6), 0.2, {strokeDashoffset: 0}, "+=.25")
+              .to(paths.eq(7), 0.15, {strokeDashoffset: 0})
+              .to(paths.eq(8), 0.15, {strokeDashoffset: 0})
+              .to(paths.eq(9), 0.15, {strokeDashoffset: 0})
+              .to(paths.eq(10), 0.15, {strokeDashoffset: 0})
+              ;
+
+        }, 500);
+
+    });
+
+
+    jQuery('.anim-item').each(function(i, item) {
+        var $item = jQuery(item);
+
+        var tlItems = new TimelineMax();
+
+            tlItems.set($item, {
+                opacity: 0,
+                scale: 0.95
+            });
+
+        var delay = 200+(i*400);
+        setTimeout(function() {
+
+            tlItems.to($item, 1, {
+                opacity: 1,
+                scale: 1,
+                ease: Power2.easeOut
+            });
+
+        }, delay);
+
+    });
+
+    // tilt();
+
+    jQuery('body').mousemove( function(event){ tilt(event); }  );
+    if(window.DeviceOrientationEvent) {
+        window.ondeviceorientation = function(event){ tilt(event); };
+    }
+
+
+
+
+
+
+var initPhotoSwipeFromDOM = function(gallerySelector) {
+
+    // parse slide data (url, title, size ...) from DOM elements
+    // (children of gallerySelector)
+    var parseThumbnailElements = function(el) {
+        var thumbElements = el.querySelectorAll('a'),
+            numNodes = thumbElements.length,
+            items = [],
+            figureEl,
+            linkEl,
+            size,
+            item;
+
+        for(var i = 0; i < numNodes; i++) {
+
+            // figureEl = thumbElements[i]; // <figure> element
+
+            // // include only element nodes
+            // if(figureEl.nodeType !== 1) {
+            //     continue;
+            // }
+
+            // linkEl = figureEl.children[0]; // <a> element
+            linkEl = thumbElements[i]; // <a> element
+
+            size = linkEl.getAttribute('data-size').split('x');
+
+            // create slide object
+            item = {
+                src: linkEl.getAttribute('href'),
+                w: parseInt(size[0], 10),
+                h: parseInt(size[1], 10)
+            };
+
+
+
+            // if(linkEl.children.length > 1) {
+            //     // <figcaption> content
+            //     item.title = linkEl.children[1].innerHTML;
+            // }
+
+            if(linkEl.children.length > 0) {
+                // <img> thumbnail element, retrieving thumbnail url
+                item.msrc = linkEl.children[0].getAttribute('src');
+            }
+
+            item.el = figureEl; // save link to element for getThumbBoundsFn
+            items.push(item);
+        }
+
+        return items;
+    };
+
+    // find nearest parent element
+    var closest = function closest(el, fn) {
+        return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+    };
+
+    // triggers when user clicks on thumbnail
+    var onThumbnailsClick = function(e) {
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+        var eTarget = e.target || e.srcElement;
+        // var clickedListItem = this;
+
+        // find root element of slide
+        // var clickedListItem = closest(eTarget, function(el) {
+        //     return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+        // });
+
+        // if(!clickedListItem) {
+        //     return;
+        // }
+
+        // find index of clicked item by looping through all child nodes
+        // alternatively, you may define index via data- attribute
+        // var clickedGallery = clickedListItem.parentNode,
+        //     childNodes = clickedListItem.parentNode.querySelectorAll('a'),
+        //     numChildNodes = childNodes.length,
+        //     nodeIndex = 0,
+        //     index;
+
+        // for (var i = 0; i < numChildNodes; i++) {
+        //     console.log('childNodes[i] : ', childNodes[i]);
+        //     if(childNodes[i].nodeType !== 1) {
+        //         continue;
+        //     }
+
+        //     if(childNodes[i] === clickedListItem) {
+        //         index = nodeIndex;
+        //         break;
+        //     }
+        //     nodeIndex++;
+        // }
+
+        clickedGallery = this;
+        index = 1;
+
+        if(index >= 0) {
+            // open PhotoSwipe if valid index found
+            openPhotoSwipe( index, clickedGallery );
+        }
+        return false;
+    };
+
+    // parse picture index and gallery index from URL (#&pid=1&gid=2)
+    var photoswipeParseHash = function() {
+        var hash = window.location.hash.substring(1),
+        params = {};
+
+        if(hash.length < 5) {
+            return params;
+        }
+
+        var vars = hash.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            if(!vars[i]) {
+                continue;
+            }
+            var pair = vars[i].split('=');
+            if(pair.length < 2) {
+                continue;
+            }
+            params[pair[0]] = pair[1];
+        }
+
+        if(params.gid) {
+            params.gid = parseInt(params.gid, 10);
+        }
+
+        return params;
+    };
+
+    var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+        var pswpElement = document.querySelectorAll('.pswp')[0],
+            gallery,
+            options,
+            items;
+
+        items = parseThumbnailElements(galleryElement);
+
+        // define options (if needed)
+        options = {
+
+            history: false,
+            shareEl: false,
+
+            // define gallery index (for URL)
+            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+            getThumbBoundsFn: function(index) {
+                // See Options -> getThumbBoundsFn section of documentation for more info
+                // var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+                //     pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                //     rect = thumbnail.getBoundingClientRect();
+
+                // return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+            }
+
+        };
+
+        // PhotoSwipe opened from URL
+        if(fromURL) {
+            if(options.galleryPIDs) {
+                // parse real index when custom PIDs are used
+                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+                for(var j = 0; j < items.length; j++) {
+                    if(items[j].pid == index) {
+                        options.index = j;
+                        break;
+                    }
+                }
+            } else {
+                // in URL indexes start from 1
+                options.index = parseInt(index, 10) - 1;
+            }
+        } else {
+            options.index = parseInt(index, 10);
+        }
+
+        // exit if index not found
+        if( isNaN(options.index) ) {
+            return;
+        }
+
+        if(disableAnimation) {
+            options.showAnimationDuration = 0;
+        }
+
+        // Pass data to PhotoSwipe and initialize it
+        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+    // loop through all gallery elements and bind events
+    var galleryElements = document.querySelectorAll( gallerySelector );
+
+    for(var i = 0, l = galleryElements.length; i < l; i++) {
+        galleryElements[i].setAttribute('data-pswp-uid', i+1);
+        // console.log('galleryElements[i].querySelectorAll(a) : ', galleryElements[i].querySelectorAll('a'));
+        var galleryElementsTrigger = galleryElements[i].querySelector('.pswp-trigger');
+        galleryElementsTrigger.onclick = onThumbnailsClick;
+
+        // for(var j = 0, lA = galleryElementsA.length; j < lA; j++) {
+        //     galleryElementsA[j].onclick = onThumbnailsClick;
+        // }
+
+
+    }
+
+    // Parse URL and open gallery if it contains #&pid=3&gid=1
+    // var hashData = photoswipeParseHash();
+    // if(hashData.pid && hashData.gid) {
+    //     openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+    // }
+};
+
+
+
+    initPhotoSwipeFromDOM('.pswp-gallery');
+
+
+
+});
+
+
+// history: false,
+// shareEl: false
+// index: currentIndex,
+// focus: false,
+// modal: false,
+// showAnimationDuration: 0,
+// hideAnimationDuration: 0,
+// closeEl: false,
+// captionEl: false,
+// fullscreenEl: false,
+// preloaderEl: false,
+// bgOpacity: 0,
+// zoomEl: false,
+// counterEl: false,
+// arrowKeys: false,
+// tapToClose: false,
+// tapToToggleControls: false,
+// clickToCloseNonZoomable: false,
+// spacing: 0,
+// closeOnScroll: false,
+// closeOnVerticalDrag: false
+
+
+function tilt(event) {
+    cx = Math.ceil(jQuery(window).width() / 2.0);
+    cy = Math.ceil(jQuery(window).height() / 2.0);
+    dx = event.pageX - cx;
+    dy = event.pageY - cy;
+    // mouseX = event.pageX;
+    // wRecetas = Math.ceil((event.pageX*(jQuery(window).width()-1920))/jQuery(window).width());
+    // minus = ((wRecetas)*(event.pageX))*-1;
+    // moveX = ((mouseX*wRecetas)*-1);
+    // console.log('wRecetas : ', wRecetas);
+    tiltx = - (dy / cy) * 1.5;
+    tilty = (dx / cx) * 1;
+    radius = Math.sqrt(Math.pow(tiltx,2) + Math.pow(tilty,2));
+    degree = (radius * 5);
+  //   console.log('tiltx : ', tiltx);
+  //   console.log('tilty : ', tilty);
+  // console.log('degree : ', degree);
+
+    TweenLite.set(".anim-tilt", {transform:'rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)'});
+    TweenLite.set(".anim-tilt-reverse", {transform:'rotate3d(' + (-tiltx) + ', ' + (-tilty) + ', 0, ' + degree + 'deg)'});
+}
+
+
+
+
--- wp-content/themes/dubeaujeuv2014/pages/e3mosaic.php
+++ wp-content/themes/dubeaujeuv2014/pages/e3mosaic.php
@@ -2,79 +2,137 @@
 
 // CSS
 
-function fon_styles_e32015mosaic() { wp_enqueue_style( 'e32015mosaic', ASSETS_URL.'/css/pages/e32015mosaic.css' ); }
+function fon_styles_e32015mosaic() {
+    wp_enqueue_style( 'photoswipe', ASSETS_URL.'/css/vendor/photoswipe.css' );
+    wp_enqueue_style( 'photoswipe-default-skin', ASSETS_URL.'/css/vendor/default-skin/default-skin.css' );
+    wp_enqueue_style( 'e32015mosaic', ASSETS_URL.'/css/pages/e32015mosaic.css' );
+}
 add_action( 'wp_enqueue_scripts', 'fon_styles_e32015mosaic' );
 
-function fon_scripts_e32015mosaic() { wp_enqueue_script( 'e32015mosaic', ASSETS_URL.'/js/pages/e32015mosaic.js', '', '1.0', true ); }
+function fon_scripts_e32015mosaic() {
+    wp_enqueue_script( 'packery', ASSETS_URL.'/js/vendor/packery.pkgd.min.js', '', '1.4.1', true );
+    wp_enqueue_script( 'photoswipe', ASSETS_URL.'/js/vendor/photoswipe.min.js', '', '1.0', true );
+    wp_enqueue_script( 'photoswipe-ui', ASSETS_URL.'/js/vendor/photoswipe-ui-default.min.js', '', '1.0', true );
+    wp_enqueue_script( 'e32015mosaic', ASSETS_URL.'/js/pages/e32015mosaic.js', '', '1.0', true );
+}
 add_action( 'wp_enqueue_scripts', 'fon_scripts_e32015mosaic' );
 
 // VIEW
 
 get_header();
+the_post();
 ?>
 <div class="e3mosaic-page">
-    <div class="bg"></div>
-    <div class="logo-box">
-        <?php require_once TEMPLATEPATH.'/assets/img/e32015/logo.svg'; ?>
-    </div>
+    <!-- <div class="bg anim-tilt-reverse"></div> -->
 
-    <div class="e32015-planning">
-        <div class="items">
-            <div class="item">
-                <div class="title">Bethesda</div>
-                <div class="infos">
-                    <div class="hours">15 juin 3:30 (France)</div>
-                    <a class="link" href="http://www.twitch.tv/bethesda" target="_blank">twitch.tv/bethesda</a>
+    <?php
+    if( have_rows('items') ): ?>
+        <div class="bricks js-hidden">
+            <?php while ( have_rows('items') ) : the_row();
+                $title = get_sub_field('title');
+                $thumbnail = get_sub_field('thumbnail');
+                $images = get_sub_field('images');
+                $format = get_sub_field('format');
+                ?>
+                <div class="brick-item format-<?php echo $format ?> anim-item anim-tilt">
+                    <div class="brick-outer">
+                    <div class="brick-inner <?php if( ! empty($images) ) echo 'pswp-gallery'; ?>">
+                        <button type="button" class="button-reset pswp-trigger">
+                            <?php if( $images ): ?>
+                                <?php foreach( $images as $image ): ?>
+                                    <a href="<?php echo $image['url']; ?>" class="pswp-item" data-size="<?php echo $image['width']; ?>x<?php echo $image['height']; ?>">
+                                        <!-- <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" hidden />
+                                        <p hidden><?php echo $image['caption']; ?></p> -->
+                                    </a>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                            <div class="bg" style="background-image: url(<?php echo $thumbnail['sizes']['large']; ?>);"></div>
+                            <div class="overlay"></div>
+                            <div class="title"><?php echo $title; ?></div>
+                        </button>
+                    </div>
+                    </div>
                 </div>
-            </div>
-            <div class="item">
-                <div class="title">Microsoft</div>
-                <div class="infos">
-                    <div class="hours">15 juin 18:30 (France)</div>
-                    <a class="link" href="http://www.xbox.com/fr-FR/E3" target="_blank">xbox.com/e3</a>
+            <?php endwhile; ?>
+            <div class="brick-item format-43pct stamp brick-logo">
+                <div class="brick-outer">
+                <div class="brick-inner">
+                    <div class="logo-box">
+                        <?php require_once TEMPLATEPATH.'/assets/img/e32015/logo.svg'; ?>
+                    </div>
                 </div>
-            </div>
-            <div class="item">
-                <div class="title">Electronic Arts</div>
-                <div class="infos">
-                    <div class="hours">15 juin 22:00 (France)</div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="title">Ubisoft</div>
-                <div class="infos">
-                    <div class="hours">16 juin 00:00 (France)</div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="title">Sony</div>
-                <div class="infos">
-                    <div class="hours">16 juin 03:00 (France)</div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="title">Nintendo</div>
-                <div class="infos">
-                    <div class="hours">16 juin 18:00 (France)</div>
-                    <a class="link" href="http://e3.nintendo.com/" target="_blank">e3.nintendo.com</a>
-                </div>
-            </div>
-            <div class="item">
-                <div class="title">Square Enix</div>
-                <div class="infos">
-                    <div class="hours">16 juin 19:00 (France)</div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="title">AMD PC Gaming Show</div>
-                <div class="infos">
-                    <div class="hours">17 juin 19:00 (France)</div>
-                    <div class="hours">02:00 (France)</div>
-                    <a class="link" href="http://www.pcgamingshow.com/" target="_blank">pcgamingshow.com</a>
                 </div>
             </div>
         </div>
+    <?php endif; ?>
+
+</div>
+
+<!-- Root element of PhotoSwipe. Must have class pswp. -->
+<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+
+    <!-- Background of PhotoSwipe.
+         It's a separate element as animating opacity is faster than rgba(). -->
+    <div class="pswp__bg"></div>
+
+    <!-- Slides wrapper with overflow:hidden. -->
+    <div class="pswp__scroll-wrap">
+
+        <!-- Container that holds slides.
+            PhotoSwipe keeps only 3 of them in the DOM to save memory.
+            Don't modify these 3 pswp__item elements, data is added later on. -->
+        <div class="pswp__container">
+            <div class="pswp__item"></div>
+            <div class="pswp__item"></div>
+            <div class="pswp__item"></div>
+        </div>
+
+        <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
+        <div class="pswp__ui pswp__ui--hidden">
+
+            <div class="pswp__top-bar">
+
+                <!--  Controls are self-explanatory. Order can be changed. -->
+
+                <div class="pswp__counter"></div>
+
+                <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+
+                <button class="pswp__button pswp__button--share" title="Share"></button>
+
+                <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+
+                <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+
+                <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
+                <!-- element will get class pswp__preloader--active when preloader is running -->
+                <div class="pswp__preloader">
+                    <div class="pswp__preloader__icn">
+                      <div class="pswp__preloader__cut">
+                        <div class="pswp__preloader__donut"></div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                <div class="pswp__share-tooltip"></div>
+            </div>
+
+            <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
+            </button>
+
+            <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
+            </button>
+
+            <div class="pswp__caption">
+                <div class="pswp__caption__center"></div>
+            </div>
+
+        </div>
+
     </div>
+
 </div>
 
 <?php
